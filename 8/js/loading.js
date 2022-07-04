6939d727006dea8f4118d9b16d4a7a5a98c449bc
Keks <keks@htmlacademy.ru>
2022-07-05 01:44:53 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/loading.js
@@ -0,0 +1,42 @@
+import { findElement, getRandomArrayElement } from './utils.js';
+import { createAuthors } from './data.js';
+
+const template = findElement(document, '#picture');
+const templatePicture = findElement(template.content, '.picture');
+const templatePictureFragment = document.createDocumentFragment();
+
+createAuthors.forEach(({ url, comments, likes }) => {
+  const photosElement = templatePicture.cloneNode(true);
+  const pictureImg = findElement(photosElement, '.picture__img');
+  pictureImg.src = url;
+  const pictureComments = findElement(photosElement, '.picture__comments');
+  pictureComments.textContent = comments.length;
+  const pictureLikes = findElement(photosElement, '.picture__likes');
+  pictureLikes.textContent = likes;
+  templatePictureFragment.appendChild(photosElement);
+}
+);
+const loadingPictures = findElement(document, '.pictures')
+loadingPictures.appendChild(templatePictureFragment);
+
+// Найти  шаблон <template id="picture">
+//Создать элемент
+// src = url
+// class="picture__likes" = likes
+// class="picture__comments" = comments
+// Добавить в DOM
+// DocumentFragment
+
+// Отобразить фотографии других пользователей.
+
+//     Заведите модуль, который будет отвечать за отрисовку миниатюр.
+
+//     На основе временных данных для разработки и шаблона #picture создайте DOM - элементы, соответствующие фотографиям, и заполните их данными:
+//         Адрес изображения url подставьте как атрибут src изображения.
+//         Количество лайков likes выведите в блок.picture__likes.
+//         Количество комментариев comments выведите в блок.picture__comments.
+
+//     Отрисуйте сгенерированные DOM - элементы в блок.pictures.Для вставки элементов используйте DocumentFragment.
+
+//     Подключите модуль в проект.
+
