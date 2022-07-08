ac9ac4fe69792d35e07449d38ad8bcac036fefc8
Damir <102433257+Damir747@users.noreply.github.com>
2022-07-08 13:18:02 +0300

    начал делать 8.2 без 8.1

--- js/picture.js
+++ js/picture.js
@@ -8,7 +8,6 @@ const modalWindow = findElement(document, '.big-picture'); // само окно
 const pictureImg = findElement(modalWindow, '.big-picture__img');
 const pictureCancel = findElement(modalWindow, '#picture-cancel');
 const likesCount = findElement(modalWindow, '.likes-count');
-const commentsCount = findElement(modalWindow, '.comments-count');
 const socialComment = findElement(modalWindow, '.social__comment-count');
 const commentsLoader = findElement(modalWindow, '.comments-loader');
 
@@ -16,6 +15,8 @@ const socialHeader = findElement(modalWindow, '.social__header');
 const socialPicture = findElement(socialHeader, '.social__picture');
 const socialCaption = findElement(socialHeader, '.social__caption');
 const socialLikes = findElement(socialHeader, '.likes-count');
+
+const socialComments = findElement(document, '.social__comments');
 
 // templatePicture.addEventListener('click', openModal);
 
@@ -42,17 +43,55 @@ function closeModal() {
   body.removeEventListener('keydown', onModalEscKeyDown);
 }
 
-function renderPicture(image) {
-  // временная штука
-  // 3. После открытия окна спрячьте блоки счётчика комментариев.social__comment - count и загрузки новых комментариев.comments - loader, добавив им класс hidden, с ними мы разберёмся позже, в другом домашнем задании.
-  socialComment.classList.add('hidden');
-  commentsLoader.classList.add('hidden');
+// 0 из 7
+// 5 из 7
+// 7 из 7
 
+// общее число комментариев. Показываю первые пять. По нажатию еще пять.
+// все комментарии закрыты, кнопка скрыта.
+// Вызов (первый вызов): показать пять
+// если комментариев ноль - всё блокирую: ничего не делаю
+// если комментариев не больше 5: открываю оставшиеся комментарии, кнопку скрываю, число показанных комментариев = кол-ву комментариев
+//  иначе (комментариев больше 5): открываю пять комментариев, кнопка открыта, число показанных комментариев += 5
+let countShownComments = 0;
+
+function show5Comments(id) {
+  const sumComments = post(id).comments.length;
+  const difference = sumComments - countShownComments;
+  if (difference <= 0) {
+    console.log('Нечего показывать');
+  } else {
+    if (difference <= 5) {
+      console.log('Показать оставшиеся ' + difference);
+      const socialComments = findElement(document, '.social__comments');
+      for (let i = countShownComments; i < sumComments - 1; i++) {
+        socialComments.childNodes[i].classList.remove('hidden');
+      }
+      countShownComments = sumComments;
+      commentsLoader.classList.add('hidden');
+    } else {
+      console.log('Показать 5 из ' + difference);
+      for (let i = countShownComments; i < countShownComments + 5; i++) {
+        socialComments.childNodes[i - 1].classList.remove('hidden');
+      }
+      countShownComments += 5;
+      commentsLoader.classList.remove('hidden');
+    };
+  }
+  let strTemp = socialComment.textContent;
+  strTemp = strTemp.slice(strTemp.indexOf(' из'));
+  socialComment.textContent = countShownComments + strTemp;
+}
+commentsLoader.addEventListener('click', show5Comments); // а если засунуть внутрь обновления картинки, то повиснет куча обработчиков, надо вешать на onclick
+
+function renderPicture(image) {
   // перерисовка новой картинки
   const img = findElement(pictureImg, 'img');
   img.src = image.src;
   img.alt = image.alt;
   likesCount.textContent = image.likes;
+  const commentsCount = findElement(modalWindow, '.comments-count');
+  console.log(commentsCount);
   commentsCount.textContent = image.comments;
   const id = image.id.slice(('picture-').length);
   socialPicture.src = post(id).avatar;
@@ -60,7 +99,6 @@ function renderPicture(image) {
   socialLikes.textContent = post(id).likes;
 
 
-  const socialComments = findElement(document, '.social__comments');
   const oneSocialComment = findElement(socialComments, '.social__comment');
   const templateCommentFragment = document.createDocumentFragment();
 
@@ -73,11 +111,14 @@ function renderPicture(image) {
     message.textContent = comments.message;
     templateCommentFragment.appendChild(templateSocialComment);
   });
+
+  // Что-то перестала роботать кнопка escape после одного закрытия
   // Удаление всех дочерних элементов
   while (socialComments.firstChild) {
     socialComments.removeChild(socialComments.firstChild);
   }
   socialComments.appendChild(templateCommentFragment);
+  show5Comments(id);
 }
 
 pictureCancel.addEventListener('click', () => {
